f399419f1281210d471ea33fe8e09afbbd1b97c0
tatyanakarlen <99438264+tatyanakarlen@users.noreply.github.com>
2022-07-09 23:59:09 -0400

    form works

--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Widget
+from django.views.generic.edit import CreateView
+from .forms import AddWidgetForm
 
 # Create your views here.
 def home(request):
   widgets = Widget.objects.all()
-  return render(request, 'index.html', {'widgets': widgets})
+  add_widget_form = AddWidgetForm()
+  return render(request, 'index.html', {'widgets': widgets, 'add_widget_form': add_widget_form})
+
+class widget_create(CreateView):
+  model = Widget
+  fields = '__all__'
+  success_url = ''
 
 
