69c18c7423ea30ca2885ad814039f72b0f3b7da7
tatyanakarlen <99438264+tatyanakarlen@users.noreply.github.com>
2022-07-10 08:51:36 -0400

    latest

--- main_app/models.py
+++ main_app/models.py
@@ -1,7 +1,13 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class Widget(models.Model):
   description = models.CharField(max_length=100)
   quantity = models.IntegerField()
+
+  def __str__(self):
+        return self.description
+
+
   
--- main_app/urls.py
+++ main_app/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
- path('', views.home, name='home'),
+ path('', views.home, name='home'), 
+ path('wackywidgets/', views.index, name='wacky_widgets'),
+ path('wackywidgets/new_widget/', views.widget_create.as_view(), name='widget_create'),
 ]
