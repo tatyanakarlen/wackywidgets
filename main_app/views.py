4600b05bfb329377097dea667299de1ce7d09f89
tatyanakarlen <99438264+tatyanakarlen@users.noreply.github.com>
2022-07-09 23:40:20 -0400

    created model

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Widget
 
 # Create your views here.
 def home(request):
-  return render(request, 'landing.html')
+  widgets = Widget.objects.all()
+  return render(request, 'index.html', {'widgets': widgets})
+
+
